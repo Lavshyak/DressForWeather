3b68b68450eb54eb04aebf26d714fd161c2c6980
DashboardPank <62081080+zhangir22@users.noreply.github.com>
2023-07-07 17:08:52 +0600

    Add files via upload
    ClotchController add
    DressReportController add
    WeatherController add

---
+++ DressForWeather.WebAPI/Controllers/ClotchController.cs
@@ -0,0 +1,41 @@
+﻿using DressForWeather.DbContextTemplates;
+using Microsoft.AspNetCore.Mvc;
+using DressForWeather.Models;
+using DressForWeather.Models.EFCoreModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace DressForWeather.WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ClotchController : Controller
+{
+    private readonly ILogger<ClotchController> _logger;
+    private readonly WeatherDressDbContext _dbContext;
+
+    public ClotchController(ILogger<ClotchController> logger, WeatherDressDbContext db) 
+    {
+        _logger = logger;
+        _dbContext = db;
+    }
+    [HttpPost]
+    public async Task<Clotch> CreateClotch
+    [HttpGet]
+    public async Task<Clotch?> GetClotchById(long id) 
+    {
+        var clotch = await _dbContext.Clotches.FirstOrDefaultAsync(c => c.Id == id);
+        return clotch;
+    }
+    [HttpGet]
+    public async Task<Clotch?> GetClotchByNama(string name) 
+    {
+        var clotch = await _dbContext.Clotches.FirstOrDefaultAsync(c=>c.Name == name);
+        return clotch;
+    }
+    [HttpGet]
+    public async Task<Clotch?> GetClotchByType(string type) 
+    {
+        var clotch = await _dbContext.Clotches.FirstOrDefaultAsync(c => c.Type == type);
+        return clotch;
+    }
+}
--- DressForWeather.WebAPI/Controllers/DressReportController.cs
+++ DressForWeather.WebAPI/Controllers/DressReportController.cs
@@ -1,30 +1,54 @@
-using DressForWeather.WebAPI.BackendModels.EFCoreModels;
-using DressForWeather.WebAPI.DbContexts;
-using Microsoft.AspNetCore.Authorization;
+using DressForWeather.DbContextTemplates;
+using DressForWeather.Models.EFCoreModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
+using DressForWeather.Models.EFCoreModels.DressRepairModels;
 namespace DressForWeather.WebAPI.Controllers;
 
 // TODO: дописать контроллер, написать недостающие контроллеры
 [ApiController]
 [Route("[controller]")]
-[Authorize]
 public class DressReportController : ControllerBase
 {
 	private readonly ILogger<DressReportController> _logger;
-	private readonly MainDbContext _mainDbContext;
-	public DressReportController(ILogger<DressReportController> logger, MainDbContext mainDbContext)
+	private readonly WeatherDressDbContext _weatherDressDbContext;
+	private IDressRepos DressRepos { get; set; }
+	public DressReportController(ILogger<DressReportController> logger, WeatherDressDbContext weatherDressDbContext,IDressRepos dressRepos)
 	{
 		_logger = logger;
-		_mainDbContext = mainDbContext;
+		_weatherDressDbContext = weatherDressDbContext;
+		DressRepos = dressRepos;
 	}
 
-	[Authorize]
-	[HttpGet(Name = "GetDressReportById")]
-	public async Task<DressReport?> GetDressReportById(long id)
+	[HttpGet(Name = "GetAllDressReport")]
+	public async List<Task<DressReport>> GetAll() 
 	{
-		var report = await _mainDbContext.DressReports.FirstOrDefaultAsync(dr => dr.Id == id);
+		var repots = await DressRepos.GetAllDressReport();
+		return repots;
+
+	}
+	[HttpGet(Name = "GetDressReportById")]
+	public async Task<DressReport?> Get(long id)
+	{
+		var report = await _weatherDressDbContext.DressReports.FirstOrDefaultAsync(dr => dr.Id == id);
 		return report;
+	}
+	[HttPost(Name = "CreateDressReport")]
+	public async Task<DressReport> Create(DressReport model) 
+	{
+		var report = await DressRepos.Create(model);
+		return report; 
+	}
+	[HttpPut(Name = "UpdateDressReport")]
+	public async Task<DressReport> Update(int id, DressReport model) 
+	{
+		bool done = false;
+		var report = await GetDressById(id);
+		if(report != null) 
+		{
+			report = DressRepos.Update(model);
+			done = true;
+		}
+		return done ? report : null;
 	}
 }
