52bdbbf2405e6a5219a9eb9b59d91311ea2343a6
DashboardPank <62081080+zhangir22@users.noreply.github.com>
2023-07-07 17:09:35 +0600

    Update DressReportController.cs

--- DressForWeather.WebAPI/Controllers/DressReportController.cs
+++ DressForWeather.WebAPI/Controllers/DressReportController.cs
@@ -1,54 +1,21 @@
-using DressForWeather.DbContextTemplates;
-using DressForWeather.Models.EFCoreModels;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using DressForWeather.Models.EFCoreModels.DressRepairModels;
-namespace DressForWeather.WebAPI.Controllers;
-
-// TODO: дописать контроллер, написать недостающие контроллеры
 [ApiController]
 [Route("[controller]")]
+[Authorize]
 public class DressReportController : ControllerBase
 {
 	private readonly ILogger<DressReportController> _logger;
-	private readonly WeatherDressDbContext _weatherDressDbContext;
-	private IDressRepos DressRepos { get; set; }
-	public DressReportController(ILogger<DressReportController> logger, WeatherDressDbContext weatherDressDbContext,IDressRepos dressRepos)
+	private readonly MainDbContext _mainDbContext;
+	public DressReportController(ILogger<DressReportController> logger, MainDbContext mainDbContext)
 	{
 		_logger = logger;
-		_weatherDressDbContext = weatherDressDbContext;
-		DressRepos = dressRepos;
+		_mainDbContext = mainDbContext;
 	}
 
-	[HttpGet(Name = "GetAllDressReport")]
-	public async List<Task<DressReport>> GetAll() 
-	{
-		var repots = await DressRepos.GetAllDressReport();
-		return repots;
-
-	}
+	[Authorize]
 	[HttpGet(Name = "GetDressReportById")]
-	public async Task<DressReport?> Get(long id)
+	public async Task<DressReport?> GetDressReportById(long id)
 	{
-		var report = await _weatherDressDbContext.DressReports.FirstOrDefaultAsync(dr => dr.Id == id);
+		var report = await _mainDbContext.DressReports.FirstOrDefaultAsync(dr => dr.Id == id);
 		return report;
-	}
-	[HttPost(Name = "CreateDressReport")]
-	public async Task<DressReport> Create(DressReport model) 
-	{
-		var report = await DressRepos.Create(model);
-		return report; 
-	}
-	[HttpPut(Name = "UpdateDressReport")]
-	public async Task<DressReport> Update(int id, DressReport model) 
-	{
-		bool done = false;
-		var report = await GetDressById(id);
-		if(report != null) 
-		{
-			report = DressRepos.Update(model);
-			done = true;
-		}
-		return done ? report : null;
 	}
 }
